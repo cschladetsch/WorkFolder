cc78c06bf405b09b92c298ddbc7fe36bfcc5c1c4
Christian <christian.schladetsch@gmail.com>
2019-07-31 20:15:38 +1000

    Add ColorFormat.cs (files need to be force-added in liminal).

---
+++ src/go/go/ColorFormat.cs
@@ -0,0 +1,26 @@
+﻿namespace go
+{
+    public static class ColorFormat
+    {
+        public static string Default = "\x1b[39m";
+        public static string Black = "\x1b[30m";
+        public static string Red = "\x1b[31m";
+        public static string Green = "\x1b[32m";
+        public static string Yellow = "\x1b[33m";
+        public static string Blue = "\x1b[34m";
+        public static string Magenta = "\x1b[35m";
+        public static string Cyan = "\x1b[36m";
+        public static string LightGrey = "\x1b[37m";
+        public static string DarkGrey = "\x1b[90m";
+        public static string LightRed = "\x1b[91m";
+        public static string LightGreen = "\x1b[92m";
+        public static string LightYellow = "\x1b[93m";
+        public static string LightBlue = "\x1b[94m";
+        public static string LightMagenta = "\x1b[95m";
+        public static string LightCyan = "\x1b[96m";
+        public static string White = "\x1b[97m";
+
+        public static string Bold = "\x1b[1m";
+        public static string Dim = "\x1b[2m";
+    }
+}

f3e2f9f443e82d83ab87f8573ec0084f0ed814f4
Christian <christian.schladetsch@gmail.com>
2019-08-13 23:02:28 +1000

    Clean src/status tree.

--- src/status/status/Program.cs
+++ src/status/status/Program.cs
@@ -1,23 +1,27 @@
-﻿using System.IO;
-using System.Linq;
-using NGit;
-using NGit.Api;
-using NGit.Storage.File;
-using NGit.Util;
-using Sharpen;
-using Console = System.Console;
+﻿using System.Collections.Generic;
+using System.Text;
+using NGit.Diff;
+using Tamir.SharpSsh.java.lang;
 
 namespace status
 {
-
     using System;
-    using System.Collections.Generic;
+    using System.Linq;
+    using NGit.Api;
+    using NGit.Util;
+    using Sharpen;
+
     using static System.IO.Directory;
     using static System.IO.Path;
-    using static Console;
+    using static System.Console;
 
+    /// <summary>
+    /// DOC
+    /// </summary>
     internal class Program
     {
+        private bool _verbose;
+
         private static int Main(string[] args)
         {
             try
@@ -26,21 +30,24 @@ namespace status
             }
             catch (Exception e)
             {
-                WriteLine(e);
+                Console.Error.WriteLine(e);
             }
 
             return -1;
         }
 
-        private int Run(IReadOnlyList<string> args)
+        private int Run(string[] args)
         {
+            if (args.Length > 0 && args[0] == "-v")
+                _verbose = true;
+
             return GetRepos();
         }
 
         private int GetRepos()
         {
             var dirs = GetDirectories(GetCurrentDirectory());
-            var repos = dirs.Where(d => Directory.Exists(Combine(d, ".git"))).ToList();
+            var repos = dirs.Where(d => Exists(Combine(d, ".git"))).ToList();
 
             foreach (var dir in dirs)
                 if (repos.Contains(dir))
@@ -52,21 +59,45 @@ namespace status
         private void GetStatus(string dir)
         {
             Write($"{dir}: ");
-            Git git = Git.Open(new FilePath(dir), FS.Detect(false));
-            Repository repository = git.GetRepository();
+            var git = Git.Open(new FilePath(dir), FS.Detect(false));
+            var repository = git.GetRepository();
             repository.ScanForRepoChanges();
-            Status status = git.Status().Call();
+            var status = git.Status().Call();
 
-            var modified = status.GetModified().Any();
-            var added = status.GetAdded().Any();
-            var removed = status.GetRemoved().Any();
-            var changed = status.GetChanged().Any();
-            var missing = status.GetMissing().Any();
-            var untracked = status.GetUntracked().Any();
+            var mods = status.GetModified();
+            var adds = status.GetAdded();
+            var rem = status.GetRemoved();
+            var changed = status.GetChanged();
+            var untracked = status.GetUntracked();
+            var missing = status.GetMissing();
 
-            var outOfDate = modified || added || removed || changed || missing || untracked;
+            var outOfDate = mods.Any() || adds.Any() || rem.Any() || changed.Any()
+                || missing.Any() || untracked.Any();
 
             WriteLine(outOfDate ? $"\x1b[2m\x1b[91mLocal has been modified.\x1b[0m" : $"\x1b[2mNo changes to local.\x1b[0m");
+            if (outOfDate && _verbose)
+            {
+                Info($"Modified", mods);
+                Info($"Added", adds);
+                Info($"Removed", rem);
+                Info($"Changed", changed);
+                Info($"Missing", missing);
+                Info($"Untracked", untracked);
+            }
+        }
+
+        private static void Info(string title, ICollection<string> mods)
+        {
+            if (mods.Count == 0)
+                return;
+
+            var sb = new StringBuilder();
+            sb.Append($"\t{title}:\n");
+            foreach (var item in mods)
+                sb.AppendLine($"\t\t{item}");
+
+            Write(sb.ToString());
         }
     }
 }
+

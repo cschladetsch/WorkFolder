248b01bcc752e8dba6c93b47d605c3bc419489fa
Christian <christian.schladetsch@gmail.com>
2020-03-31 23:39:52 +1100

    Add default Csharp Unity script.

---
+++ doc/default/Csharp-Script.cs
@@ -0,0 +1,30 @@
+namespace App
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    /// <summary>
+    /// What this class does and how it works with other classes.
+    /// </summary>
+    public class #SCRIPTNAME#
+        : MonoBehaviour
+    {
+        private void Awake()
+        {
+            #NOTRIM#
+        }
+
+        private void Start()
+        {
+            #NOTRIM#
+        }
+
+        private void Update()
+        
+            #NOTRIM#
+        }
+    }
+}
+
